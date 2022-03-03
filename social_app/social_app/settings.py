c845decdf0890441821e46f3b15f5fc55ea4a3f6
Hamza Shafiq <hamzashafique054@gmail.com>
2022-03-03 22:32:11 +0500

    abstract api integrated

--- social_app/social_app/settings.py
+++ social_app/social_app/settings.py
@@ -146,3 +146,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GEO_API_KEY = os.getenv("GEO_API_KEY")
+HD_API_KEY = os.getenv("HD_API_KEY")
+EV_API_KEY = os.getenv("EV_API_KEY")
---
+++ social_app/utils/abstarct_api.py
@@ -0,0 +1,63 @@
+import requests
+import json
+from django.conf import settings
+
+
+class AbstractAPI:
+    def __init__(self):
+        self.GEO_API_KEY = settings.GEO_API_KEY
+        self.HD_API_KEY = settings.HD_API_KEY
+        self.EV_API_KEY = settings.EV_API_KEY
+        self.BASE_URL = "https://{}.abstractapi.com/v1/"
+
+    def get_geolocation_content(self):
+        url = self.BASE_URL.format("ipgeolocation")
+        payload = "{}?api_key={}".format(url, self.GEO_API_KEY)
+        response = requests.get(payload)
+        resp = json.loads(response.content)
+
+        result_set = {
+            "ip_address": resp["ip_address"],
+            "city": resp["city"],
+            "region": resp["region"],
+            "country": resp["country"],
+            "country_code": resp["country_code"],
+            "continent": resp["continent"],
+            "postal_code": resp["postal_code"],
+            "latitude": resp["latitude"],
+            "longitude": resp["longitude"],
+            "country_flag": resp["flag"]["png"]
+        }
+
+        return result_set
+
+    @staticmethod
+    def extract_info(param):
+        return {
+            "event_title": param["name"],
+            "week_day": param["week_day"],
+        }
+
+    def get_holidays(self, country_code, date):
+        url = self.BASE_URL.format("holidays")
+        payload = "{}?api_key={}&country={}&year={}&month={}&day={}".format(
+            url, self.HD_API_KEY, country_code, date["year"], date["month"], date["day"])
+        response = requests.get(payload)
+        resp_list = json.loads(response.content)
+
+        holiday_info = [self.extract_info(each_holiday) for each_holiday in resp_list]
+        return holiday_info
+
+    def validate_email(self, email):
+        url = self.BASE_URL.format("emailvalidation")
+        payload = "{}?api_key={}&email={}".format(url, self.EV_API_KEY, email)
+
+        response = requests.get(payload)
+        resp = json.loads(response.content)
+
+        email_validity = {
+            "deliverability": resp["deliverability"],
+            "is_valid_format": resp["is_valid_format"]["value"],
+            "is_smtp_valid": resp["is_smtp_valid"]["value"]
+        }
+        return email_validity
