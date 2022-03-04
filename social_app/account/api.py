aab491ba729710e3adf8446d34725b2c2236e9b7
Hamza Shafiq <hamzashafique054@gmail.com>
2022-03-05 01:39:09 +0500

    few updates

--- social_app/account/api.py
+++ social_app/account/api.py
@@ -1,8 +1,9 @@
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, status
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from .serializer import RegisterSerializer, UserSerializer
-from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
+from utils.abstarct_api import AbstractAPI
 
 from .tasks import save_user_location
 from django_q.tasks import async_task
@@ -12,23 +13,31 @@ class RegisterApi(generics.GenericAPIView):
     serializer_class = RegisterSerializer
 
     def post(self, request, *args,  **kwargs):
+        # Validate email address through abstract-api
+        abstract_api = AbstractAPI()
+        valid_email = abstract_api.validate_email(email=request.data.get("email"))
+        if not valid_email["is_valid_format"]:
+            content = {'message': 'invalid email address!'}
+            return Response(content, status=status.HTTP_400_BAD_REQUEST)
+
+        # Create new user record
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
-
         user_data = UserSerializer(user, context=self.get_serializer_context()).data
 
         # Run Async Task - Abstract API to fetch information
         async_task(save_user_location, user_data["id"])
 
-        return Response({
-            "user": user_data,
-            "message": "User Created Successfully. Now perform Login to get your token",
-        })
+        content = {
+            "data": user_data,
+            "message": "User Created Successfully. Now perform Login to get your token"
+        }
+        return Response(content, status=status.HTTP_201_CREATED)
 
 
 # ViewSets define the view behavior.
 class UserViewSet(viewsets.ModelViewSet):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = User.objects.all()
     serializer_class = UserSerializer
